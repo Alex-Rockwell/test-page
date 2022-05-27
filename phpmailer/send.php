2c6c3d187d75d4b853f5872342a8951510e96e22
unknown <vin-ag@list.ru>
2022-05-27 10:28:43 +0400

    SMTP-270522

--- phpmailer/send.php
+++ phpmailer/send.php
@@ -2,6 +2,7 @@
 // header('Access-Control-Allow-Origin: *');
 use PHPMailer\PHPMailer\PHPMailer;
 use Exception\Exception\Exception;
+use SMTP\SMTP\SMTP;
 
 require 'PHPMailer.php';
 require 'SMTP.php';
@@ -11,35 +12,36 @@ require 'Exception.php';
 // require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
-$mail->IsHTML(true);
+// $mail->IsHTML(true);
 
 // $mail->SMTPDebug = 3;                              // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'ssl://smtp.mail.ru';                   // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';                         // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'test6331402@inbox.ru';             // Наш логин
-$mail->Password = 'rtxopnm1402';                      // Наш пароль от ящика
+$mail->Username = 'test2205271@mail.ru';              // Наш логин
+$mail->Password = '36r0g64A8SiW08sGczHd';             // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('mail@gmail.com', 'Сайт');             // От кого письмо 
-$mail->addAddress('mail@yandex.ru');                  // Add a recipient
+$mail->setFrom('test2205271@mail.ru', 'Сайт');        // От кого письмо 
+$mail->addAddress('test2205271@mail.ru');             // Add a recipient
 $mail->isHTML(true);                                  // Set email format to HTML
 
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$org = $_POST['org'];
+// $name = $_POST['name'];
+// $phone = $_POST['phone'];
+// $email = $_POST['email'];
+// $org = $_POST['org'];
 
 $mail->Subject = 'Данные';
-$mail->Body    = '
-    Пользователь оставил данные <br> <br>
-    Имя: ' . $name . ' <br><br>
-    Номер телефона: ' . $phone . '<br><br>
-    E-mail: ' . $email . '<br><br>
-    Org: ' .$org . '';
+$mail->Body    = 'Данные';
+// $mail->Body    = '
+//     Пользователь оставил данные <br> <br>
+//     Имя: ' . $name . ' <br><br>
+//     Номер телефона: ' . $phone . '<br><br>
+//     E-mail: ' . $email . '<br><br>
+//     Org: ' .$org . '';
 
 if(!$mail->send()) {
     return false;
